3c73f0f3ac3bddc527b812b7f90ee46996a445e4
SavO <zasadna@gmail.com>
2026-04-06 23:06:03 -0500

    Create stable working UI layout for HabitTracker with sidebar navigation and pages before Materio integration

--- HabitTrackerAspNetMVCWebApp/obj/Debug/net10.0/HabitTrackerAspNetMVCWebApp.AssemblyInfo.cs
+++ HabitTrackerAspNetMVCWebApp/obj/Debug/net10.0/HabitTrackerAspNetMVCWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HabitTrackerAspNetMVCWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9106cd3bac35a6c057e627a00c1ca244334281f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8685f2d3b5ca572aace5c89e4fc91c030191beca")]
 [assembly: System.Reflection.AssemblyProductAttribute("HabitTrackerAspNetMVCWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HabitTrackerAspNetMVCWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
