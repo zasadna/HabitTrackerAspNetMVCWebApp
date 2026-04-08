3565308928751cafaa17a52f1450b9bca3b7e77b
SavO <zasadna@gmail.com>
2026-04-08 18:30:43 -0500

    Add Habits Index view and fix layout sidebar navigation

--- HabitTrackerAspNetMVCWebApp/obj/Debug/net10.0/HabitTrackerAspNetMVCWebApp.AssemblyInfo.cs
+++ HabitTrackerAspNetMVCWebApp/obj/Debug/net10.0/HabitTrackerAspNetMVCWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HabitTrackerAspNetMVCWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c73f0f3ac3bddc527b812b7f90ee46996a445e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+353500470d7f5936da707b5ebedccf356c0887a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HabitTrackerAspNetMVCWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HabitTrackerAspNetMVCWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
