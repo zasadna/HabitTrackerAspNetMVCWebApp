efdeb0e2183a1bae3abc1b25c3e5ecba4e91fe56
SavO <zasadna@gmail.com>
2026-04-08 19:45:35 -0500

    feat: add Create Habit page and fix TagHelpers/bootstrap integration

--- HabitTrackerAspNetMVCWebApp/Program.cs
+++ HabitTrackerAspNetMVCWebApp/Program.cs
@@ -1,39 +1,32 @@
 using HabitTrackerAspNetMVCWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 
-namespace HabitTrackerAspNetMVCWebApp;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddControllersWithViews();
-        builder.Services.AddHttpContextAccessor();
-
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(
-                builder.Configuration.GetConnectionString("DefaultConnection")));
-
-        var app = builder.Build();
-
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Home/Error");
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.MapStaticAssets();
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-
-        app.Run();
-    }
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
--- HabitTrackerAspNetMVCWebApp/obj/Debug/net10.0/HabitTrackerAspNetMVCWebApp.AssemblyInfo.cs
+++ HabitTrackerAspNetMVCWebApp/obj/Debug/net10.0/HabitTrackerAspNetMVCWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HabitTrackerAspNetMVCWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+353500470d7f5936da707b5ebedccf356c0887a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3565308928751cafaa17a52f1450b9bca3b7e77b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HabitTrackerAspNetMVCWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HabitTrackerAspNetMVCWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
