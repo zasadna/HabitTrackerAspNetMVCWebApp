379546c800602699402dce83f382d901a79c5982
SavO <zasadna@gmail.com>
2026-04-13 19:47:35 -0500

    Fix dashboard stats to show user habits

--- HabitTrackerAspNetMVCWebApp/Controllers/HomeController.cs
+++ HabitTrackerAspNetMVCWebApp/Controllers/HomeController.cs
@@ -1,34 +1,60 @@
 ﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using HabitTrackerAspNetMVCWebApp.Data;
 using HabitTrackerAspNetMVCWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace HabitTrackerAspNetMVCWebApp.Controllers;
-
-public class HomeController : Controller
+namespace HabitTrackerAspNetMVCWebApp.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel
+        public HomeController(
+            ILogger<HomeController> logger,
+            ApplicationDbContext context)
         {
-            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-        });
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (User.Identity?.IsAuthenticated != true)
+            {
+                return View(new List<Habit>());
+            }
+
+            var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrWhiteSpace(currentUserId))
+            {
+                return View(new List<Habit>());
+            }
+
+            var habits = await _context.Habits
+                .Include(h => h.User)
+                .Where(h => h.UserId == currentUserId)
+                .OrderByDescending(h => h.StartDate)
+                .ToListAsync();
+
+            return View(habits);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
+        }
     }
 }
--- HabitTrackerAspNetMVCWebApp/obj/Debug/net10.0/HabitTrackerAspNetMVCWebApp.AssemblyInfo.cs
+++ HabitTrackerAspNetMVCWebApp/obj/Debug/net10.0/HabitTrackerAspNetMVCWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HabitTrackerAspNetMVCWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2e48fee8e51cc0f9a196d63034a847fcbef7b88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85bc52a372cf226945c34c11b7b0b52b3a1d1859")]
 [assembly: System.Reflection.AssemblyProductAttribute("HabitTrackerAspNetMVCWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HabitTrackerAspNetMVCWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
